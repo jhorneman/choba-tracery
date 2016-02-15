da95dcfbc7d14808ebd3cb1ecd83f29e54b9b02f
Jurie Horneman <jurie@jurie.org>
2016-02-15 15:51:05 +0100

    Rough integration into Choba.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 var tracery = require('./tracery/tracery');
+var choba = require('choba-engine');
+
+
+function evaluateTraceryExpression(_parameters, _dynamicState, _context) {
+    // Check if context has grammars.
+    // Check if there are two parameters.
+    // Check if grammars has desired grammar.
+    var grammar = _context.grammars[_parameters[0]];
+    var trace = grammar.expand(_parameters[1]);
+    // Check if Tracery returned errors.
+    return {
+        type: 'string',
+        value: trace.finishedText
+    };
+}
+
 
 var grammar = tracery.createGrammar({
   'animal': ['panda','fox','capybara','iguana'],
@@ -8,7 +24,22 @@ var grammar = tracery.createGrammar({
 
 grammar.addModifiers(tracery.baseEngModifiers);
 
-var trace = grammar.expand('#origin#');
+var context = choba.buildContext({
+    scenes: {
+        start: {
+            content: ['seq',
+                ['text', 'Hello! '],
+                ['trace', 'test', '#origin#']
+            ]
+        }
+    },
+    grammars: {
+        test: grammar
+    },
+    expressionEvaluators: Object.assign({}, choba.expressionEvaluators, {
+        'trace': evaluateTraceryExpression
+    })
+});
 
-console.log(trace.finishedText);
-console.log(trace.errors);
+var rv = choba.restartGame(context);
+console.log(rv.newScene.text);
